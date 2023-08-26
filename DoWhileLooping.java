aee4268cc3ba9785f10d7662291de6885a12396e
raghavakv <90055640+raghavakv@users.noreply.github.com>
2023-08-26 20:10:07 +0530

    Add files via upload

---
+++ DoWhileLooping.java
@@ -0,0 +1,13 @@
+package com.looping;
+
+public class DoWhileLooping {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 100);
+	}
+
+}
---
+++ ForLooping.java
@@ -0,0 +1,11 @@
+package com.looping;
+
+public class ForLooping {
+	public static void main(String[] args) {
+		// int i = 0;
+		for (int i = 200; i < 500; i += 2) {
+			System.out.println(i);
+		}
+	}
+
+}
