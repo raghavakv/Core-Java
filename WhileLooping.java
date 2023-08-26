24dfbf4e534142f5d0cc0ea03a21a2163ce0452b
raghavakv <90055640+raghavakv@users.noreply.github.com>
2023-08-26 20:01:24 +0530

    Add files via upload

---
+++ WhileLooping.java
@@ -0,0 +1,13 @@
+package com.looping;
+
+public class WhileLooping {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 100) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
