b4e40b6fbde0efa02612b03e38ce5352f1c39206
raghavakv <90055640+raghavakv@users.noreply.github.com>
2023-09-02 15:33:16 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package com.identifiers;
+
+public class PrimitiveDataTypes {
+	static byte defaultByte;
+	static short defaultShort;
+	static int defaultInt;
+	static long defaultLong;
+	static float defaultFloat;
+	static double defaultDouble;
+	static boolean defaultBoolean;
+	static char defaultCharacter;
+
+	public static void main(String[] args) {
+
+		System.out.println("Default value of byte: " + defaultByte);
+		System.out.println("Default value of short: " + defaultShort);
+		System.out.println("Default value of int: " + defaultInt);
+		System.out.println("Default value of long: " + defaultLong);
+		System.out.println("Default value of float: " + defaultFloat);
+		System.out.println("Default value of double: " + defaultDouble);
+		System.out.println("Default value of boolean: " + defaultBoolean);
+		System.out.println("Default value of char: " + defaultCharacter);
+	}
+
+}
