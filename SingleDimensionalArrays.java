e3b16d0e810a7d85ec719656605491ab8ff4d42b
raghavakv <90055640+raghavakv@users.noreply.github.com>
2023-09-03 22:31:00 +0530

    Add files via upload

---
+++ SingleDimensionalArrays.java
@@ -0,0 +1,11 @@
+package com.arrays.one;
+
+public class SingleDimensionalArrays {
+
+	public static void main(String[] args) {
+		int[] j = {00,10,20,30,40,50,60,70,80,90};
+		// System.out.println(j[0]+j[2]+j[3]+j[4]);
+		for (int i = 0; i < j.length; i++)
+		System.out.println(j[i]);
+		}
+	}
